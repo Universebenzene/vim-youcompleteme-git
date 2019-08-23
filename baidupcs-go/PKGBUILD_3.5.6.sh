e8060ff8a7a3e8eec927a0b243e2b433ddc2dec2
Universebenzene <universebenzene@sina.com>
2019-08-23 12:47:51 +0800

    Correct permission of doc

--- baidupcs-go/PKGBUILD_3.5.6.sh
+++ baidupcs-go/PKGBUILD_3.5.6.sh
@@ -9,6 +9,7 @@ arch=('x86_64')
 _importpath="github.com/iikira/${_pkgname}"
 url="https://${_importpath}"
 license=('Apache')
+depends=('glibc')
 makedepends=('go-pie' 'git')
 conflicts=("baidupcs")
 provides=("baidupcs")
@@ -16,20 +17,20 @@ source=("${pkgname}-${pkgver}.tar.gz::${url}/archive/v${pkgver}.tar.gz")
 sha512sums=('fcec0e07b984dd83e554c98866db771737284c3ce1321e4fa05137e5d7ae59f5d4d1280024984dab3a5aa0a097312d0454c6d0d16d97c823d97342c44c8ca31e')
 
 prepare() {
-  cd "${srcdir}"
+    cd "${srcdir}"
 
-  mkdir -p .gopath/src/github.com/iikira
-  ln -rTsf "${_pkgname}-${pkgver}" ".gopath/src/${_importpath}"
-  export GOPATH="${srcdir}/.gopath:/usr/share/gocode"
+    mkdir -p .gopath/src/github.com/iikira
+    ln -rTsf "${_pkgname}-${pkgver}" ".gopath/src/${_importpath}"
+    export GOPATH="${srcdir}/.gopath:/usr/share/gocode"
 }
 
 build() {
-  cd "${srcdir}"
-  go build -v -work -x "${_importpath}"
+    cd "${srcdir}"
+    go build -v -work -x "${_importpath}"
 }
 
 package() {
-  cd "${srcdir}"
-  install -Dm755 "${_pkgname}" "${pkgdir}/usr/bin/baidupcs"
-  install -Dm644 "${_pkgname}-${pkgver}/README.md" -t "${pkgdir}/usr/share/doc/${pkgname}"
+    cd "${srcdir}"
+    install -Dm755 "${_pkgname}" "${pkgdir}/usr/bin/baidupcs"
+    install -Dm644 "${_pkgname}-${pkgver}/README.md" -t "${pkgdir}/usr/share/doc/${pkgname}"
 }
